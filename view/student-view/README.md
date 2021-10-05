9c8cb2f26439f58bdc942e1653b6fb472f695307
Leonid Shuliak <lshuliak@exadel.com>
2021-10-05 22:59:09 +0300

    init: angular

--- view/student-view/README.md
+++ view/student-view/README.md
@@ -1,4 +1,4 @@
-# StudentProject
+# StudentView
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.8.
 
--- view/student-view/src/app/app.component.spec.ts
+++ view/student-view/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'student-project'`, () => {
+  it(`should have as title 'student-view'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('student-project');
+    expect(app.title).toEqual('student-view');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('student-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('student-view app is running!');
   });
 });
